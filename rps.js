56949d9aedf6bc9f21573f9d5304530150c81786
marcobollettini <55404322+marcobollettini@users.noreply.github.com>
2019-10-28 17:54:02 +0100

    kind of done

--- rps.js
+++ rps.js
@@ -1,31 +1,80 @@
+let win = 0;
+let loss = 0;
+let draw = 0;
+const sRound = document.querySelector('#sRound');
+const aiScore = document.querySelector('#aiScore');
+const yourScore = document.querySelector('#yourScore');
+const choices = document.querySelectorAll('.choices');
+yourScore.textContent = 0;
+aiScore.textContent = 0;
+
+
 //scelta random del pc
 
 const pcPlay = () => {
-    let choices = ['rock', 'paper', 'scissors']
-    let result = choices[Math.floor(Math.random() * choices.length)];
+    let aiChoices = ['rock', 'paper', 'scissors']
+    let result = aiChoices[Math.floor(Math.random() * aiChoices.length)];
     return result;
 }
 
 //selezione del giocatore
-const choose = () => {
-    let pImput = prompt("rock, paper or scissors?");
-    /*   if (pImput == null || undefined) {
-          choose();
-      } */
-    if (pImput.toLowerCase() === "rock" || pImput.toLowerCase() === "paper" || pImput.toLowerCase() === "scissors") {
-        pImput = pImput.toLowerCase();
-        return pImput
 
-    }
-    else {
 
-    }
-    //else choose(); */
-}
+
+
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+
+        let youSelect = choice.id;
+        let pcSelect = pcPlay();
+        console.log(youSelect, pcSelect);
+        console.log(playRound(youSelect, pcSelect));
+        playRound(youSelect, pcSelect);
+
+        // PARTITA A 5
+
+        if (playRound(youSelect, pcSelect) === "win") {
+            win++
+            console.log(win, loss);
+            yourScore.textContent = win;
+            sRound.textContent = `you chose ${youSelect}, the computer chose ${pcSelect}: you ${playRound(youSelect, pcSelect)}!`;
+            if (win >= 5) {
+                alert('you won');
+                win = 0;
+                loss = 0;
+                yourScore.textContent = win;
+                aiScore.textContent = loss;
+
+            }
+        }
+        else if (playRound(youSelect, pcSelect) === "lose") {
+            loss++
+            console.log(win, loss);
+            aiScore.textContent = loss;
+            sRound.textContent = `you chose ${youSelect}, the computer chose ${pcSelect}: you ${playRound(youSelect, pcSelect)}!`;
+            if (loss >= 5) {
+                alert('you lose');
+                win = 0;
+                loss = 0;
+                aiScore.textContent = loss;
+                yourScore.textContent = win;
+            }
+        }
+        else {
+            console.log(win, loss);
+            sRound.textContent = `you chose ${youSelect}, the computer chose ${pcSelect}: you ${playRound(youSelect, pcSelect)}!`;
+        }
+
+    });
+
+});
 
 
 //svolgimeto del ROUND
+
+
 const playRound = (youSelect, pcSelect) => {
+
     if (youSelect === "rock") {
         if (pcSelect === "scissors") {
             let roundres = "win";
@@ -75,38 +124,8 @@ const playRound = (youSelect, pcSelect) => {
 
 }
 
-let win = 0;
-let loss = 0;
-let draw = 0;
 
 
-// RIPETI 5 VOLTE I ROUND PER FARE UN GAME
-for (i = 0; i < 5; i++) {
-    const pcSelect = pcPlay();
-    const youSelect = choose();
-    const round = playRound(youSelect, pcSelect);
-    if (round === "win") {
-        win++;
-    }
-    else if (round === "lose") {
-        loss++;
-    }
-    else {
-        draw++;
-    }
-    console.log(`you chose ${youSelect}, the computer chose ${pcSelect}: you ${round}!`);
-}
 
-//DECIDE CHI HA VINTO
-const gameWinner = (win, loss) => {
-    if (win > loss) {
-        return "YOU WON!"
-    }
-    else if (loss > win) {
-        return "YOU LOSE :("
-    }
-    else {
-        return "IT'S A DRAW"
-    }
-}
-console.log(gameWinner(win, loss))
+
+
